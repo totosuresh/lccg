43028261e963f8132120a8214b25c4385e9d0e6a
suresh Pedireddi <letsureshknow@gmail.com>
2023-07-10 19:38:35 -0400

    pushing update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
+//import profileImage from './img/laura.jp'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Loudoun Cricket grounds</h1>
     </div>
   );
 }
